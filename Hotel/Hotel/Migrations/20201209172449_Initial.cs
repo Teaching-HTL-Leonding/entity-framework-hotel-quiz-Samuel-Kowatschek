19abf7795386b21a54f4313e04ac58b408c63c86
Samuel Kowatschek <samkow@gmx.at>
2020-12-09 22:21:47 +0100

    started creating tables

---
+++ Hotel/Hotel/Migrations/20201209172449_Initial.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel_Exercise.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Hotels",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hotels", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HotelSpecials",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Specials = table.Column<int>(type: "int", nullable: false),
+                    HotelId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HotelSpecials", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HotelSpecials_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RoomTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(75)", maxLength: 75, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Size = table.Column<int>(type: "int", nullable: false),
+                    IsDisabilityAccessible = table.Column<bool>(type: "bit", nullable: false),
+                    NummberOfAvailableRooms = table.Column<int>(type: "int", nullable: false),
+                    HotelId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RoomTypes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RoomTypes_Hotels_HotelId",
+                        column: x => x.HotelId,
+                        principalTable: "Hotels",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Prices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoomTypeId = table.Column<int>(type: "int", nullable: false),
+                    ValidFrom = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ValidTo = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PriceEUR = table.Column<decimal>(type: "decimal(8,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Prices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Prices_RoomTypes_RoomTypeId",
+                        column: x => x.RoomTypeId,
+                        principalTable: "RoomTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HotelSpecials_HotelId",
+                table: "HotelSpecials",
+                column: "HotelId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Prices_RoomTypeId",
+                table: "Prices",
+                column: "RoomTypeId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RoomTypes_HotelId",
+                table: "RoomTypes",
+                column: "HotelId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HotelSpecials");
+
+            migrationBuilder.DropTable(
+                name: "Prices");
+
+            migrationBuilder.DropTable(
+                name: "RoomTypes");
+
+            migrationBuilder.DropTable(
+                name: "Hotels");
+        }
+    }
+}
---
+++ Hotel/Hotel/Program.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+using System.Threading.Tasks;
+
+//GEMEINSAM MIT HUEMER PATRICK GECODED
+
+
+var factory = new HotelContextFactory();
+using var dbContext = factory.CreateDbContext();
+
+if (args[0] == "add")
+{
+    await AddData();
+}
+else if (args[0] == "query")
+{
+    await QueryData();
+}
+
+async Task AddData()
+{
+    
+}
+
+async Task QueryData()
+{
+    
+}
+
+#region Model
+enum Special        //The different hotel specials
+{
+    Spa,
+    Sauna,
+    DogFriendly,
+    IndoorPool,
+    OutdoorPool,
+    BikeRental,
+    ECarChargingStation,
+    VegetarianCuisine,
+    OrganicFood
+}
+class Hotel
+{
+    public int Id { get; set; }
+    [MaxLength(100)]
+    public string Name { get; set; } = "";
+    [MaxLength(100)]
+    public string Address { get; set; } = "";
+    public List<HotelSpecial> Special { get; set; } = new();
+    public List<RoomType> RoomTypes { get; set; } = new();
+}
+class HotelSpecial
+{
+    public int Id { get; set; }
+    public Special Special { get; set; }
+    public Hotel? Hotel { get; set; }
+}
+class RoomType
+{
+    public int Id { get; set; }
+    [MaxLength(75)]
+    public string Title { get; set; } = string.Empty;
+    public string? Description { get; set; }
+    public int Size { get; set; }
+    public bool IsDisabilityAccessible { get; set; }
+    public int NummberOfAvailableRooms { get; set; }
+    public Hotel? Hotel { get; set; }
+    public int HotelId { get; set; }
+    public Price? Price { get; set; }
+}
+class Price
+{
+    public int Id { get; set; }
+    public RoomType? RoomType { get; set; }
+    public int RoomTypeId { get; set; }
+    public DateTime ValidFrom { get; set; }
+    public DateTime ValidTo { get; set; }
+    [Column(TypeName = "decimal(8, 2)")]
+    public decimal PriceEUR { get; set; }
+}
+#endregion
+
+#region Context
+class HotelContext : DbContext
+{
+    public DbSet<Hotel> Hotels { get; set; }
+    public DbSet<HotelSpecial> HotelSpecial { get; set; }
+    public DbSet<RoomType> RoomTypes { get; set; }
+    public DbSet<Price> Prices { get; set; }
+#pragma warning disable CS8618
+    public HotelContext(DbContextOptions<HotelContext> options) : base(options) { }
+#pragma warning restore CS8618 
+
+}
+class HotelContextFactory : IDesignTimeDbContextFactory<HotelContext>
+{
+    public HotelContext CreateDbContext(string[]? args = null)
+    {
+        var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+        var optionsBuilder = new DbContextOptionsBuilder<HotelContext>();
+        optionsBuilder
+            .UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+        return new HotelContext(optionsBuilder.Options);
+    }
+}
+#endregion
